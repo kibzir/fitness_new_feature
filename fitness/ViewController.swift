034d64ebe1d837450f6b1f273cab4c28ab0e4531
Vlad Kozyr <kibzir@mail.ru>
2021-04-22 22:25:30 +0300

    Initial Commit

---
+++ fitness/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  fitness
+//
+//  Created by Владислав Козырь on 22.04.21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
